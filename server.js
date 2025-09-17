a18847c13ba89695ea55178cb0db21488a8159b9
Anjali Priya <anjali@example.com>
2025-09-17 21:01:18 +0530

    Resolved merge conflict in server.js

--- server.js
+++ server.js
@@ -9,16 +9,17 @@ const donationRoutes = require("./routes/donationRoutes");
 const app = express();
 
 // Middleware
-<<<<<<< HEAD
-app.use(cors({
-    origin: "http://localhost:3000", // Allow requests only from localhost frontend
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
+//<<<<<<< HEAD
+//<<<<<<< HEAD
+// app.use(cors({
+//     origin: "http://localhost:3000", // Allow requests only from localhost frontend
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true
+// }));
 
-app.use(express.json()); // For parsing JSON requests
+// app.use(express.json()); // For parsing JSON requests
 
-=======
+//=======
 
 app.use(cors({
     origin: ["https://reshare-app.vercel.app", "http://localhost:3000"],
@@ -26,7 +27,12 @@ app.use(cors({
     credentials: true
 }));
 app.use(express.json());
->>>>>>> fe22fd041b5d495f9e98571bbc9676b16e09512e
+//>>>>>>> fe22fd041b5d495f9e98571bbc9676b16e09512e
+//=======
+// app.use(cors({ origin: "https://reshare-app.vercel.app", methods: ["GET","POST","PUT","DELETE"],
+//   credentials: true }));
+// app.use(express.json());
+//>>>>>>> 1f864fb17cf7b8433765229f9cf1fd5f6d0bee50
 // Routes
 app.use("/api/donations", donationRoutes);
 app.use("/api/auth", authRoutes);
